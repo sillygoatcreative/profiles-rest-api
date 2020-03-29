d03da045be6e3aafb4b7045d5044f54167b94afc
Rick R <rick@mmrr.biz>
2020-03-29 08:06:33 -0400

    added custom user model

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'profiles_project',
+    'profiles_api',
     ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
